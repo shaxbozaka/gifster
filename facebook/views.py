f7727221b94a91f89f1eb0b16154a2b1cbdeb2f8
shaxbozaka <shaxboz824@icloud.com>
2022-06-21 15:39:39 +0500

    ...

--- facebook/views.py
+++ facebook/views.py
@@ -7,10 +7,8 @@ class FriendsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        # username = self.request.data.get('username')
-        # pwd = self.request.data.get('pwd')
-        print(request.headers)
-        # response = fb2cal.get_Friends(username, pwd)
-
-        return Response(request.headers, status=200)
+        username = self.request.data.get('username')
+        pwd = self.request.data.get('pwd')
+        response = fb2cal.get_Friends(username, pwd)
+        return Response(response, status=response['error'])
 
