547ae2b71421dab83fff36b6e7713875b3b6c941
shaxbozaka <shaxboz824@icloud.com>
2022-06-21 16:03:15 +0500

    ..

--- Giftster/settings.py
+++ Giftster/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
